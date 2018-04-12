afcdb7f12b501dca4a19dbbe2c8f5ba186ed5258
duskcloudxu <cmbjx1@qq.com>
2018-04-12 21:36:55 +0800

    finalagain?

--- src/page/manage.vue
+++ src/page/manage.vue
@@ -5,41 +5,29 @@
 				<el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
 					<el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
 					<el-submenu index="2">
-						<template slot="title"><i class="el-icon-document"></i>数据管理</template>
-						<el-menu-item index="userList">用户列表</el-menu-item>
-						<el-menu-item index="shopList">商家列表</el-menu-item>
-						<el-menu-item index="foodList">食品列表</el-menu-item>
-						<el-menu-item index="orderList">订单列表</el-menu-item>
-						<el-menu-item index="adminList">管理员列表</el-menu-item>
+						<template slot="title"><i class="el-icon-document"></i>合同模板</template>
+						<el-menu-item index="userList">模板库</el-menu-item>
+						<el-menu-item index="shopList">我的模板</el-menu-item>
+						<el-menu-item index="foodList">委托起草</el-menu-item>
 					</el-submenu>
 					<el-submenu index="3">
-						<template slot="title"><i class="el-icon-plus"></i>添加数据</template>
-						<el-menu-item index="addShop">添加商铺</el-menu-item>
-						<el-menu-item index="addGoods">添加商品</el-menu-item>
+						<template slot="title"><i class="el-icon-plus"></i>帮审合同</template>
 					</el-submenu>
 					<el-submenu index="4">
-						<template slot="title"><i class="el-icon-star-on"></i>图表</template>
-						<el-menu-item index="visitor">用户分布</el-menu-item>
-						<!-- <el-menu-item index="newMember">用户数据</el-menu-item> -->
+						<template slot="title"><i class="el-icon-star-on"></i>合同管理</template>
+						<el-menu-item index="document">全部文件</el-menu-item>
+                        <el-menu-item index="">待自己签</el-menu-item>
+                        <el-menu-item index="">待他人签</el-menu-item>
+                        <el-menu-item index="">已完成</el-menu-item>
+                        <el-menu-item index="">已过期</el-menu-item>
+                        <el-menu-item index="">已撤销</el-menu-item>
+                        <el-menu-item index="">已拒签</el-menu-item>
+                        <el-menu-item index="">草稿箱</el-menu-item>
 					</el-submenu>
-					<el-submenu index="5">
-						<template slot="title"><i class="el-icon-edit"></i>编辑</template>
-						<!-- <el-menu-item index="uploadImg">上传图片</el-menu-item> -->
-						<el-menu-item index="vueEdit">文本编辑</el-menu-item>
-					</el-submenu>
-					<el-submenu index="6">
-						<template slot="title"><i class="el-icon-setting"></i>设置</template>
-						<el-menu-item index="adminSet">管理员设置</el-menu-item>
-						<!-- <el-menu-item index="sendMessage">发送通知</el-menu-item> -->
-					</el-submenu>
-					<el-submenu index="7">
-						<template slot="title"><i class="el-icon-warning"></i>说明</template>
-						<el-menu-item index="explain">说明</el-menu-item>
-					</el-submenu>
-                    <el-submenu index="8">
-                        <template slot="title"><i class="el-icon-document"></i>合同查看</template>
-                        <el-menu-item index="document">合同查看</el-menu-item>
+                    <el-submenu index="4">
+                        <template slot="title"><i class="el-icon-setting"></i>账号设置</template>
                     </el-submenu>
+
 				</el-menu>
 			</el-col>
 			<el-col :span="20" style="height: 100%;overflow: auto;">
